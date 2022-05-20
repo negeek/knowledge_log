43809c0092b99412135bb4158d635644b4b4b907
negeek <negeek5ive@gmail.com>
2022-05-20 12:49:13 +0100

    styled the entry page

--- logs/forms.py
+++ logs/forms.py
@@ -1,18 +1,22 @@
+from logging import PlaceHolder
 from django import forms
 from matplotlib import widgets
 from .models import Topic, Entry
 
+
 class Topicform(forms.ModelForm):
     class Meta:
-        model=Topic
-        fields=['topic', 'public', 'description'] #only text field is displayed from Topic
-        labels={'topic':''} #signifies no label for text field
-        widgets={'description':forms.Textarea(attrs={'cols':80})}
-      
+        model = Topic
+        # only text field is displayed from Topic
+        fields = ['topic', 'public', 'description']
+        labels = {'topic': ''}  # signifies no label for text field
+        widgets = {'description': forms.Textarea(attrs={'rows': 3})}
+
 
 class EntryForm(forms.ModelForm):
     class Meta:
-        model=Entry
-        fields=['entry']
-        labels={'entry':''}
-        widgets={'entry':forms.Textarea(attrs={'cols':80})}
+        model = Entry
+        fields = ['entry']
+        labels = {'entry': ''}
+        widgets = {'entry': forms.Textarea(
+            attrs={'rows': 3, "PlaceHolder": "Today, i learnt about..."})}
