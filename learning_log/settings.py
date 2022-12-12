4d6b6982535e4bc53047a0acd1fa9f9a0e9bfa9b
negeek <negeek5ive@gmail.com>
2022-12-12 17:43:00 +0100

    configure to deploy on doprax 9

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://knowledgeloggqhy.eu-ccofhtfzmvsd.dopraxapp.com']
